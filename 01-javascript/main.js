20e1d85791b05184796c74dba52d572da75ebdb1
Miguel Vega <miguel.vega@gmail.com>
2025-10-22 15:41:08 +0200

    chore(delete logs and duplicates)

--- 01-javascript/main.js
+++ 01-javascript/main.js
@@ -2,13 +2,9 @@ import { state } from './config.js'
 
 import './fetch-data.js'
 import './filters.js'
-import './filters.js'
-import './filters.js'
-import './filters.js'
-import './filters.js'
 import './apply-button.js'
 import './devjobs-avatar-element.js'
 
 state.count++
 
-console.log(state)
+// console.log(state)
