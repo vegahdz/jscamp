525673aeb33ac3cfcce1dcd87df2fe66614c2be4
Miguel Vega <miguel.vega@gmail.com>
2025-10-22 15:43:44 +0200

    feat(add searcher filter results)

--- 01-javascript/filters.js
+++ 01-javascript/filters.js
@@ -2,7 +2,7 @@ import { state } from './config.js'
 
 state.count++
 
-console.log(state)
+// console.log(state)
 
 const filter = document.querySelector('#filter-location')
 const mensaje = document.querySelector('#filter-selected-value')
@@ -25,3 +25,25 @@ filter.addEventListener('change', function () {
     job.classList.toggle('is-hidden', isShown === false)
   })
 })
+
+
+
+const inputSearch = document.querySelector('#empleos-search-input');
+
+inputSearch.addEventListener('input', function () {
+
+  const searchValue = inputSearch.value.toLowerCase();
+  console.log(searchValue);
+
+  const jobs = document.querySelectorAll('.job-listing-card')
+
+
+  jobs.forEach(job => {
+    const titleText = job.querySelector('h3').textContent.toLowerCase()
+    // console.log(titleText);
+    const coincide = titleText.includes(searchValue);
+    console.log(coincide);
+    job.classList.toggle('is-hidden', !coincide)
+  });
+
+});
