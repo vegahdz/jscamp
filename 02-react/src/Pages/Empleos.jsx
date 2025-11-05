97541df29f690aae0e85a32672dbc13e569418f9
Miguel Vega <miguel.vega@gmail.com>
2025-11-05 14:42:38 +0100

    feat(add pagination logic states)

--- 02-react/src/Pages/Empleos.jsx
+++ 02-react/src/Pages/Empleos.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import { Header } from '@/components/Header.jsx'
 import { JobsListing } from '@/components/JobsListing.jsx'
 import { Footer } from '@/components/Footer.jsx'
 import { Pagination } from '@/components/Pagination.jsx'
 
 export function Empleos() {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const totalPages = 10;
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page)
+    }
+
     return (
         <>
             <Header />
             <JobsListing />
-            <Pagination currentPage={1} totalPages={10} />
+            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
             <Footer />
         </>
     )
--- 02-react/src/components/Pagination.jsx
+++ 02-react/src/components/Pagination.jsx
@@ -5,29 +5,63 @@ export function Pagination(props) {
 
     const {
         currentPage = 1,
+        onPageChange,
         totalPages = 1,
     } = props;
 
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
+    const isFirstPage = currentPage === 1;
+    const isLastPage = currentPage === totalPages;
+
+    const handlePrevClick = (event) => {
+        event.preventDefault();
+        if(!isFirstPage) {
+            onPageChange(currentPage - 1);
+        }
+    }
+
+    const handleNextClick = (event) => {
+        event.preventDefault();
+        if (!isLastPage) {
+            onPageChange(currentPage + 1);
+        }
+    }
+
+    const handleChangePage = (event, page) => {
+        event.preventDefault();
+        if (page !== currentPage) {
+            onPageChange(page);
+        }
+    }
+
     return (
-        <>
-            <nav className="pagination">
-                <button>
-                    <IconChevronLeft />
+        <nav className="pagination">
+
+            <button
+                disabled={currentPage === 1}
+                onClick={handlePrevClick}
+            >
+                <IconChevronLeft />
+            </button>
+
+            {pages.map(page => (
+                <button
+                    key={page}
+                    className={currentPage === page ? 'is-active': ''}
+                    onClick={(event) => handleChangePage(event, page)}
+                >
+                    {page}
                 </button>
-                {pages.map(page => (
-                    <button
-                        key={page}
-                        className={currentPage === page ? 'is-active': ''}
-                    >
-                        {page}
-                    </button>
-                ))}
-                <button>
-                    <IconChevronRight />
-                </button>
-            </nav>
-        </>
+            ))}
+
+            <button
+                disabled={currentPage === totalPages}
+                onClick={handleNextClick}
+            >
+                <IconChevronRight />
+            </button>
+
+        </nav>
     )
 }
